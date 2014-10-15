49ea201dc769a153d7c94819d5394cd37386e017
yangyzheng <yang.zheng@ieee.org>
2014-10-14 17:24:19 -0700

    updated contact model display

--- src/com/tresleches/aadp/fragment/ContactFragment.java
+++ src/com/tresleches/aadp/fragment/ContactFragment.java
@@ -1,13 +1,81 @@
 package com.tresleches.aadp.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.tresleches.aadp.R;
+import com.tresleches.aadp.adapter.ContactArrayAdapter;
+import com.tresleches.aadp.model.Contact;
 
 public class ContactFragment extends Fragment {
+	private ArrayAdapter<Contact> aContacts;
+	private ArrayList<Contact> contacts;
+
+	public ContactFragment() {
+
+	}
+
+	public static ContactFragment newInstance(String category) {
+		ContactFragment contactFragment = new ContactFragment();
+		Bundle args = new Bundle();
+		args.putString("category", category);
+		contactFragment.setArguments(args);
+		return contactFragment;
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		
 		super.onCreate(savedInstanceState);
+		contacts = new ArrayList<Contact>();
+		aContacts = new ContactArrayAdapter(getActivity(), contacts);
+		String category = getArguments().getString("category");
+		getContacts(category);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		View v = inflater.inflate(R.layout.fragment_contact, container, false);
+		ListView lvContact = (ListView) v.findViewById(R.id.lvContact);
+		lvContact.setAdapter(aContacts);
+
+		return v;
+	}
+
+	public void getContacts(String category) {
+		// Define the class we would like to query
+		ParseQuery<Contact> query = ParseQuery.getQuery(Contact.class);
+		// Define our query conditions
+		query.whereEqualTo("category", category);
+
+		query.findInBackground(new FindCallback<Contact>() {
+			public void done(List<Contact> objects, ParseException e) {
+				if (e == null) {
+					// The query was successful.
+					addAll(objects);
+				} else {
+					// Something went wrong.
+					Log.d("ERROR", e.toString());
+				}
+			}
+		});
+	}
+
+	public void addAll(List<Contact> contacts) {
+		aContacts.addAll(contacts);
 	}
 }
