88ac11813cd7f8510db05792eba1c6c8d2637ba9
neha <nehaa1812@gmail.com>
2014-10-19 10:44:51 -0700

    fixing as per review comments

--- src/com/tresleches/aadp/helper/DateHelper.java
+++ src/com/tresleches/aadp/helper/DateHelper.java
@@ -7,8 +7,7 @@ public class DateHelper {
 
 	public static String getDateInString(Date date){
 		SimpleDateFormat ddMMMyyFormat = new SimpleDateFormat("dd-MMM-yy");
-		String date_to_string = ddMMMyyFormat.format(date);
-		System.out.println("Today's date into dd-MMM-yy format: " + date_to_string);
-		return date_to_string;
+		String dateToString = ddMMMyyFormat.format(date);
+		return dateToString;
 	}
 }
